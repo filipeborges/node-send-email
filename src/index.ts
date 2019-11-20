5100588251d5f53aaa1e0f973a0d97d2d8ac4051
Filipe Borges <filipebkc2209@gmail.com>
2019-11-19 22:56:13 -0300

    Building Html email content with WebScraping result

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,31 @@ import { argv } from './config';
 import { sendEmail } from './email';
 import { readJson } from './file';
 
+interface WebScrapingResult {
+  name: string;
+  price: string;
+  detailLink: string;
+}
+
+function buildHtmlFromWebscrapingResult(json: string) {
+  const results = JSON.parse(json) as WebScrapingResult[];
+
+  let html = '';
+
+  results.forEach(result => {
+    html = html + `
+      <p>
+        <div><a href="${result.detailLink}">${result.name}</a></div>
+        <div>${result.price}</div>
+      </p>
+    `;
+  });
+
+  return html;
+}
+
 readJson(argv.file)
   .then(data => {
-    sendEmail(argv, data);
+    sendEmail(argv, buildHtmlFromWebscrapingResult(data));
   })
   .catch(err => console.error(err));
